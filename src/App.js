16a2ed6ab35bebc0678b584696f325a175287c15
porissawa <porissawa@users.noreply.github.com>
2018-12-06 08:25:45 -0200

    Added hardcoded states. Added new class to include more cards.

--- src/App.js
+++ src/App.js
@@ -9,10 +9,30 @@ const cardStyle = {
   color: '#FEFEFE',
   backgroundColor: '#444',
   padding: '1em',
-  border: 'white 1px solid'
+  border: 'white 1px solid',
+  marginBottom: '1em',
+}
+
+const itemsSpace = {
+  marginBottom: '0.8em',
 }
 
 class Dashboard extends React.Component {
+  state = {
+    pomoCards: [
+      {
+        title: 'Estudar React',
+        project: 'Web Dev',
+        countdown: '1500000',
+      },
+      {
+        title: 'Estudar JS',
+        project: 'Web Dev',
+        countdown: '1500000'
+      },
+    ]
+  }
+
   render() {
     return (
       <div style={{
@@ -24,59 +44,48 @@ class Dashboard extends React.Component {
         width: '100%',
         height: '80vh',
       }}>
-        <EditableTimerList />
-        <TimerArchive />
+        <EditablePomoCardList 
+          pomoCards={this.state.pomoCards}/>
+        <ToggleableCardForm/>
       </div>
     )
   }
 }
 
-class EditableTimerList extends React.Component {
-  pomoCards = [
-    {
-      title: 'Estudar React',
-      project: 'Web Dev',
-      countdown: '1500000',
-    },
-    {
-      title: 'Estudar JS',
-      project: 'Web Dev',
-      countdown: '1500000'
-    },
-  ]
+class EditablePomoCardList extends React.Component {
+
   render() {
+    const pomoCards = this.props.pomoCards.map((pomoCard) => (
+      <EditablePomoCard
+        title={pomoCard.title}
+        project={pomoCard.project}
+        countdown={pomoCard.countdown}
+      />
+    ))
     return (
-      <div>
-        <EditableTimer 
-        title = 'Estudar React'
-        project = 'Web Dev'
-        countdown = '1500000'
-        editFormOpen = {true}
-        />
-        
-        <EditableTimer
-        title = 'Estudar JS'
-        project = 'Web Dev'
-        countdown = '1500000'
-        editFormOpen = {false}
-        />
+      <div id='pomoCards'>
+        {pomoCards}
       </div>
     )
   }
 }
 
-class EditableTimer extends React.Component {
+class EditablePomoCard extends React.Component {
+  state = {
+    editFormOpen: false,
+  }
+
   render() {
-    if (this.props.editFormOpen) { 
+    if (this.state.editFormOpen) { 
       return (
-        <TimerForm 
+        <PomoCardForm 
           title={this.props.title} 
           project={this.props.project}
         />
       )
     } else {
       return ( 
-        <Timer
+        <PomoCard
           title={this.props.title} 
           project={this.props.project}
           countdown={this.props.countdown}
@@ -87,21 +96,48 @@ class EditableTimer extends React.Component {
 }
 
 
-class TimerForm extends React.Component {
+class PomoCardForm extends React.Component {
+  state = {
+    title: this.props.title || '',
+    project: this.props.project || '',
+    countdown: this.props.countdown || '',
+  }
+
+  handleTitleChange = (e) => {
+    this.setState({title: e.target.value})
+  }
+
+  handleProjectChange = (e) => {
+    this.setState({project: e.title.value})
+  }
+
+  handleCountdownChange = (e) => {
+    this.setState({countdown: e.target.value})
+  }
+
   render() {
     const saveCard = this.props.title ? 'Update' : 'Create'
 
     return (
-      <div style={cardStyle}>
+      <div style={{...cardStyle}}>
         <h2>Nova tarefa</h2>
-        <div style={{marginBottom: '1em'}}>
-          <input type='text' className='title' defaultValue={this.props.title}></input>
+        <div style={itemsSpace}>
+          <input type='text' 
+            className='title' 
+            value={this.state.title}
+            onChange={this.handleTitleChange} />
         </div>
-        <div style={{marginBottom: '1em'}}>
-          <input type='text' className='project' defaultValue={this.props.project}></input>
+        <div style={itemsSpace}>
+          <input type='text' 
+            className='project' 
+            value={this.state.project}
+            onChange={this.handleProjectChange} />
         </div>
-        <div style={{marginBottom: '1em'}}>
-          <input type='text' className='countdown' placeholder='00:00'></input>
+        <div style={itemsSpace}>
+          <input type='number' 
+            className='countdown' 
+            placeholder='Minutes'
+            onChange={this.handleCountdownChange} />
         </div>
         <div className='bottom buttons' style={{display: 'flex', justifyContent: 'space-between', }}>
           <button className='Save' style={{
@@ -126,38 +162,68 @@ class TimerForm extends React.Component {
   }
 }
 
-class Timer extends React.Component {
-  
+class ToggleableCardForm extends React.Component {
+  state ={
+    isOpen: false,
+  }
+
+  handleFormOpen = () => {
+    this.setState({isOpen: true})
+  }
+
+  handleFormClose = () => {
+    this.setState({ isOpen: false })
+  }
+
+  handleFormSubmit = (pomoCard) => {
+    this.props.onFormSubmit(pomoCard)
+    this.setState({isOpen: false})
+  }
+
   render() {
-    return (
-      <div style={cardStyle}>
-        <h2 className='title'>{this.props.title}</h2>
-        <h3 className='project'>{this.props.project}</h3>
-        <h2 className='countdown'>{this.props.countdown}</h2>
-      </div>
-    )
+    if(this.state.isOpen) {
+        return (
+            <PomoCardForm 
+                onFormSubmit={this.handleFormSubmit}
+                onFormClose={this.handleFormClose}/>
+        )
+    } else {
+        return (
+            <div>
+                <button 
+                    onClick={this.handleFormOpen}>
+                    <b>+</b>
+                </button>
+            </div>
+        )
+    }
   }
 }
 
-class TimerArchive extends React.Component {
+class PomoCard extends React.Component {
   render() {
     return (
       <div>
-
+        <div className='content' style={cardStyle}>
+          <div className='title' style={itemsSpace}>
+            {this.props.title}
+          </div>
+          <div className='project' style={itemsSpace}>
+            {this.props.project}
+          </div>
+          <div className='countdown' style={itemsSpace}>
+            {this.props.countdown}
+          </div>
+          <div className='extra'>
+            <button className='edit' style={{float: 'right'}}>Edit</button><br/>
+            <button className='discard' style={{float: 'right'}}>Discard</button>
+          </div>
+        </div>
       </div>
     )
   }
 }
 
-class Analysis extends React.Component {
-  render() {
-    return (
-      <div>
-
-      </div>
-    )
-  }
-}
 
 ReactDOM.render(
   <Dashboard />,
